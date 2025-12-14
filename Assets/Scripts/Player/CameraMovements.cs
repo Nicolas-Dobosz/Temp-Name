dcdc2b7796adb5290360d8301db21e02b0366c80
Kaylana1968 <144374760+Kaylana1968@users.noreply.github.com>
2025-12-14 22:08:28 +0100

    add: camera moves alongside the player

---
+++ Assets/Scripts/Player/CameraMovements.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class CameraMovements : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] [Range(0f, 1f)] float cameraSmoothing;
+
+    void FixedUpdate()
+    {
+        Vector3 targetPosition = new(player.position.x, player.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, cameraSmoothing);
+    }
+}
