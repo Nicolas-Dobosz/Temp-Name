eaa8e21bb44460382cf10cd90f8e3e2f217485cc
Nicolas <nico.dobosz@gmail.com>
2025-12-14 23:38:17 +0100

    we can use the attack button

--- Assets/Scripts/Player/PlayerMovements.cs
+++ Assets/Scripts/Player/PlayerMovements.cs
@@ -1,3 +1,4 @@
+using Mono.Cecil;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,14 +8,20 @@ public class PlayerMovements : MonoBehaviour
     [SerializeField] InputActionAsset inputActions;
 
     InputAction moveAction;
+    InputAction attackAction;
     Rigidbody2D body;
     Vector2 moveDirection;
 
     public float speed;
 
+    public WeaponData equippedWeapon;
+    private float lastAttackTime;
+    public Transform attackPoint;
+
     void Awake()
     {
         moveAction = InputSystem.actions.FindAction("Move");
+        attackAction = InputSystem.actions.FindAction("Attack");
         body = GetComponent<Rigidbody2D>();
     }
 
@@ -30,7 +37,14 @@ public class PlayerMovements : MonoBehaviour
 
     void FixedUpdate()
     {
+        attackAction.performed += context => {Attack();};
+        lastAttackTime = Time.time;
         moveDirection = moveAction.ReadValue<Vector2>();
         body.linearVelocity = speed * Time.deltaTime * moveDirection;
+    }
+
+    void Attack()
+    {
+        print("Attack");
     }
 }
